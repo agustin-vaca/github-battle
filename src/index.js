3fc900d7ae59a82f28558ec6eeaaff5d5d2b1bfd
Agustin Vaca <agustin.vaca@outlook.com>
2021-03-01 15:53:54 -0400

    About to fetch data from Github

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import Popular from './components/Popular'
+import './index.css'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Popular/>
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  <App />,
+  document.getElementById('app')
+)
