c1668eec5a27d03f4c07bbf1c9045d0063c2b8be
Agustin Vaca <agustin.vaca@outlook.com>
2021-03-04 15:59:07 -0400

    Reset implemented

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import Popular from './components/Popular'
 import './index.css'
+import Battle from './components/Battle'
 
 class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <Popular/>
+        <Battle/>
       </div>
     )
   }
